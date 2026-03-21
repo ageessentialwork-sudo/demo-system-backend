bd132b6a049c65c99d294b408fff65980d12eed4
myworld <mihensakeer@gmail.com>
2026-03-21 11:59:29 +0530

    sace

--- db.js
+++ db.js
@@ -1,11 +1,31 @@
-const mysql = require("mysql2");
-require("dotenv").config();
+// const mysql = require("mysql2");
+// require("dotenv").config();
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+// const db = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME
+// });
+
+// module.exports = db;
+
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
-module.exports = db;
+connection.connect((err) => {
+  if (err) {
+    console.error("❌ DB ERROR:", err);
+  } else {
+    console.log("✅ Connected to Railway MySQL");
+  }
+});
+
+module.exports = connection;
