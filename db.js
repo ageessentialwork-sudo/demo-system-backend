1fe22fbb34922b20797e5c577f83f943fc94e4df
myworld <mihensakeer@gmail.com>
2026-05-20 13:01:40 +0530

    migrate

--- db.js
+++ db.js
@@ -1,19 +1,28 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  port: process.env.MYSQLPORT,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
+// test connection
+db.getConnection((err, connection) => {
+
   if (err) {
     console.error("❌ DB connection failed:", err);
   } else {
-    console.log("✅ Connected to Railway MySQL 🚀");
+    console.log("✅ Connected to Hostinger MySQL 🚀");
+
+    connection.release();
   }
+
 });
 
 module.exports = db;
